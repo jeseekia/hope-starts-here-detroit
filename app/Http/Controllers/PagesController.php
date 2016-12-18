7c7f58aa94a7846433ae141409b865256c2a5774
Jeseekia Vaughn <jeseekia@gmail.com>
2016-12-18 15:56:12 -0500

    Returning views for home, about, media, resources, and connect page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function home(){
+      return view('home');
+    }
+
+    public function about(){
+      return view('about');
+    }
+
+    public function media(){
+      return view('media');
+    }
+
+    public function resources(){
+      return view('resources');
+    }
+
+    public function connect(){
+      return view('connect');
+    }
 }
