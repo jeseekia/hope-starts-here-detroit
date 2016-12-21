fbe3509d347667ae9ad5d59a88be0f86613c2f3f
Jeseekia Vaughn <jeseekia@gmail.com>
2016-12-21 00:46:02 -0500

    Remove home page route that displays posts now implemented in posts controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,10 +6,7 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function home(){
-      return view('home');
-    }
-
+  
     public function about(){
       return view('about');
     }
