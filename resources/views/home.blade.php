570d9532438465dd76cb35e48989c50da9280cf2
Jeseekia Vaughn <jeseekia@gmail.com>
2016-12-21 00:47:49 -0500

    Add html skeleton for article posts and use blade to repeat all posts in the database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layout')
 
 @section('content')
-  <h2>Home placeholder</h2>
+
+  @foreach ($posts as $post)
+    <article>
+      <img src="{{$post->thumbnail}}" alt="" />
+      <h2>{{$post->title}}</h2>
+      <h3>{{$post->subtitle}}</h3>
+      <p>
+        {{$post->body}}
+      </p>
+      <a href="{{$post->slug}}">Read more</a>
+    </article>
+  @endforeach
 @stop
