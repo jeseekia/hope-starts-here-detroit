b8f89ad82e62332c19071581c788624a77f2f814
Jeseekia Vaughn <jeseekia@gmail.com>
2016-12-20 23:50:00 -0500

    Adding the header, nav, and footer skeleton to layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,6 +13,26 @@
 
     </head>
     <body>
+        <header>
+          <a href="/">
+            <img src="#" alt="Hope Starts Here Detroit logo" />
+          </a>
+        </header>
+        <nav>
+          <ul>
+            <li><a href="/about">About</a></li>
+            <li><a href="/media">About</a></li>
+            <li><a href="/resources">About</a></li>
+            <li><a href="/connect">About</a></li>
+          </ul>
+        </nav>
+
         @yield('content')
+
+        <footer>
+          <p>
+            Copyright © 2016 Hope Starts Here: Detroit's Early Childhood Partnership
+          </p>
+        </footer>
     </body>
 </html>
