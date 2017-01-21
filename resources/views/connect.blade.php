8d2f13c5bd5466d52ea97ca99c586b062197f26b
Jeseekia Vaughn <jeseekia@gmail.com>
2017-01-21 07:05:10 -0500

    Adding a bootstrap panel around content

--- resources/views/connect.blade.php
+++ resources/views/connect.blade.php
@@ -1,5 +1,9 @@
 @extends('layout')
 
 @section('content')
-  <h2>Connect placeholder</h2>
+<div class="panel panel-default">
+  <div class="panel-body">
+    <h2>Connect placeholder</h2>
+  </div>
+</div>
 @stop
