55c8fef4724ec78db0c87e5365dbddbef7b1e051
Jeseekia Vaughn <jeseekia@gmail.com>
2017-01-21 07:13:05 -0500

    Adding bootstrap panel around content, and begin adding links to media files

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -1,5 +1,16 @@
 @extends('layout')
 
 @section('content')
-  <h2>Media placeholder</h2>
+<div class="panel panel-default">
+  <div class="panel-body">
+    <h2>MEDIA</h2>
+    <a href="https://drive.google.com/file/d/0B0U1ZK20T0G-RUdfajFkeTE0NnM/view?pli=1">Hope Starts Here Announcement Release </a>
+    <a href="#">Hope Starts Here Fact Sheet</a>
+    <a href="#">Hope Starts Here Co-Chair bios</a>
+    <a href="https://drive.google.com/file/d/0B0U1ZK20T0G-ZE1uekY2MklWVG8/view?pli=1">Hope Starts Here Stewardship Board</a>
+    <a href="#">Hope Starts Here Imagery</a>
+
+    <p>For media inquiries, please contact Nicole de Beaufort nicole@earlyworksllc.com.</p>
+  </div>
+</div>
 @stop
