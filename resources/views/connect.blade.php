20853667ab6cd2fab3e193ee380f8a8a11b4d399
Jeseekia Vaughn <jeseekia@gmail.com>
2017-02-05 22:51:36 -0500

    Open link in a new tab/window

--- resources/views/connect.blade.php
+++ resources/views/connect.blade.php
@@ -8,7 +8,7 @@
       Email: <a href="mailto:info@hopestartsheredetroit.org">info@hopestartsheredetroit.org</a>
     </p>
     <p>Phone: 313.242.7385</p>
-    <p>Facebook: <a href="https://www.facebook.com/HopeStartsHereDetroit">Follow us</a></p>
+    <p>Facebook: <a href="http://www.facebook.com/HopeStartsHereDetroit" target="_blank">Follow us</a></p>
   </div>
 </div>
 @stop
