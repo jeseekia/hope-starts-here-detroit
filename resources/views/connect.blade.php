b5439cbe59e9f5ccdfed2038bd8bc21af476b97c
Jeseekia Vaughn <jeseekia@gmail.com>
2017-02-05 22:50:17 -0500

    Adding content for page

--- resources/views/connect.blade.php
+++ resources/views/connect.blade.php
@@ -3,7 +3,12 @@
 @section('content')
 <div class="panel panel-default">
   <div class="panel-body">
-    <h2>Connect placeholder</h2>
+    <h2>Connect With Us</h2>
+    <p>
+      Email: <a href="mailto:info@hopestartsheredetroit.org">info@hopestartsheredetroit.org</a>
+    </p>
+    <p>Phone: 313.242.7385</p>
+    <p>Facebook: <a href="https://www.facebook.com/HopeStartsHereDetroit">Follow us</a></p>
   </div>
 </div>
 @stop
