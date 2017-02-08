42b4e607393ae141e4fb237cb2d285f1448da0dc
Jeseekia Vaughn <jeseekia@gmail.com>
2017-02-08 09:56:24 -0500

    Making each line a paragraph and connecting missing file links

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -4,13 +4,23 @@
 <div class="panel panel-default">
   <div class="panel-body">
     <h2>MEDIA</h2>
-    <a href="https://drive.google.com/file/d/0B0U1ZK20T0G-RUdfajFkeTE0NnM/view?pli=1">Hope Starts Here Announcement Release </a>
-    <a href="#">Hope Starts Here Fact Sheet</a>
-    <a href="#">Hope Starts Here Co-Chair bios</a>
-    <a href="https://drive.google.com/file/d/0B0U1ZK20T0G-ZE1uekY2MklWVG8/view?pli=1">Hope Starts Here Stewardship Board</a>
-    <a href="#">Hope Starts Here Imagery</a>
+    <p>
+      <a href="https://drive.google.com/file/d/0B0U1ZK20T0G-RUdfajFkeTE0NnM/view?pli=1">Hope Starts Here Announcement Release </a>
+    </p>
+    <p>
+      <a href="https://drive.google.com/file/d/0B0U1ZK20T0G-bl9yWUZmSFdhMWc/view">Hope Starts Here Fact Sheet</a>
+    </p>
+    <p>
+      <a href="https://drive.google.com/file/d/0B0U1ZK20T0G-Rmphdkxxc21DQWM/view">Hope Starts Here Co-Chair bios</a>
+    </p>
+    <p>
+      <a href="https://drive.google.com/file/d/0B0U1ZK20T0G-ZE1uekY2MklWVG8/view?pli=1">Hope Starts Here Stewardship Board</a>
+    </p>
+    <p>
+      <a href="https://drive.google.com/file/d/0B0U1ZK20T0G-dUlXS0RBNFAwSzg/view">Hope Starts Here Imagery</a>
+    </p>
 
-    <p>For media inquiries, please contact Nicole de Beaufort nicole@earlyworksllc.com.</p>
+    <p>For media inquiries, please contact Nicole de Beaufort <a href="mailto:nicole@earlyworksllc.com">nicole@earlyworksllc.com</a>.</p>
   </div>
 </div>
 @stop
