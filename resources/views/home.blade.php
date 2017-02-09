a2eac661a9de80bc9041e6c91f3e2e6a424764e2
Jeseekia Vaughn <jeseekia@gmail.com>
2017-02-09 01:12:31 -0500

    Add logic to only show image or video based on file extension

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,24 @@
 @section('content')
 
   @foreach ($posts as $post)
-  <div class="panel panel-default">
+  <div class="panel panel-default post" id="show page">
     <div class="panel-body">
-      <article>
-        <img src="/post-images/{{$post->thumbnail}}" alt="" />
-        <h2>{{$post->title}}</h2>
+      <article class="post-content">
+        @if(strlen($post->thumbnail)<=6)
+
+        @elseif($post->thumbnail)
+          <?php $extension=substr($post->thumbnail,-4,4); $extension = strtolower($extension)?>
+          @if($extension === ".jpg" || $extension === "jpeg" || $extension === ".png")
+            <img src="/post-images/{{$post->thumbnail}}" alt="" />
+          @else
+            <video src="/post-images/{{$post->thumbnail}}" autoplay>
+
+            </video>
+          @endif
+        @else
+          <p>test 2</p>
+        @endif
+        <h2 class="title">{{$post->title}}</h2>
         <h3>{{$post->subtitle}}</h3>
         <p>
           {{$post->body}}
