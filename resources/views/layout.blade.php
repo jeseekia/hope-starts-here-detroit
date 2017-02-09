e20fcc98a80d4347f97e941576d9660e2e736d05
Jeseekia Vaughn <jeseekia@gmail.com>
2017-02-09 17:34:47 -0500

    Fixing website title

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Home Starts Here Detroit</title>
+        <title>Hope Starts Here Detroit</title>
 
         <!-- Fonts -->
 
