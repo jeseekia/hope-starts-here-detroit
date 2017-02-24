1a7a0928047becaf6ff8c7c2b4898adfebcac0f7
Jeseekia Vaughn <jeseekia@gmail.com>
2017-02-24 17:20:13 -0500

    Add placeholder text until content is done

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -14,7 +14,11 @@
   <div class="panel-body">
     <!-- <h2 class="title">Resources</h2> -->
     <div class="page-content-wrapper">
-      <p>CHECK BACK SOON FOR RESOURCES!</p>
+      <p>
+        Hope Starts Here is developing a conversation toolkit that we will use together in group and neighborhood discussions about childhood well-being and education. This toolkit will help not only guide conversations we lead in the community, but conversations YOU lead as well! Check back soon for that resource.
+      </p>
+      <p>
+        Already know you want to lead a discussion? <a href="/connect">EMAIL US</a> and we will get you more information!</p>
     </div>
   </div>
 </div>
