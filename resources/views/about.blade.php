e452fa05ae91571beb0f520baffe6ed08afed721
Jeseekia Vaughn <jeseekia@gmail.com>
2017-02-24 13:07:37 -0500

    Add new content to about page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,54 +1,96 @@
 @extends('layout')
 
 @section('content')
-<div class="panel panel-default">
-  <div class="panel-body">
-    <h2 class="title">About Hope Starts Here</h2>
-    <div class="page-content-wrapper">
-      <p>
-        Hope Starts Here: Detroit’s Early Childhood Partnership is a community engagement and strategic planning process designed to create a vision and action plan to make Detroit a world-class city for our youngest residents and their families. Over the next few months, Hope Starts Here will listen to parents, business leaders, child education and health experts and many others to ensure children are born healthy, are prepared for kindergarten, and are on track for success by third grade and beyond.
-      </p>
-
-      <h3>
-        Why
-      </h3>
-      <p>
-        Children are born with nearly unlimited potential. One of the truest measures of a city’s well-being is how it helps children realize that potential, especially among the most vulnerable. Today, most of Detroit’s 80,000 young children and their families lack high-quality resources and opportunities to help them realize their full potential. Prenatal care, pediatric care, mental health services, healthy foods, high-quality early care, education programs, and safe streets and schools are too often out of reach for many Detroit families. While investments are increasing for young children, significant gaps in funding, policies and standards make it difficult to build a coordinated, high-quality system in Detroit to serve all children.
-      </p>
-
-      <h3>
-        Who
-      </h3>
-
-      <p>
-        W.K. Kellogg Foundation CEO La June Montgomery Tabron and The Kresge Foundation CEO Rip Rapson will co-chair this collective process, along with a larger group of diverse local leaders. These esteemed foundations have been trusted partners in Detroit for nearly a century, making investments in the city since their foundings. Additionally, WKKF is a national champion of leading early childhood efforts, while Kresge has engaged in place-based work in Detroit for more than a decade. These philanthropic organizations have a wealth of knowledge, expertise and resources to share with the community through Hope Starts Here.
-      </p>
-      <p>
-        IFF, a nonprofit community development finance institution, acts as the project administrator on behalf of The Kresge Foundation and the W.K. Kellogg Foundation. IFF opened an office in Detroit in early 2014 and, since then, has deployed more than $44-million in capital across Michigan, conducted two research studies in the early childhood sector, and leads real estate work on behalf of the nonprofit sector.
-      </p>
-      <p>
-        FSG, a national consulting firm that has worked in other communities to bring community-wide social change programs to life, will lead the planning work. EarlyWorks, a Detroit-based strategic communications and engagement firm, is working in partnership with FSG. A network of local, statewide and national grantees of both foundations will support this work in a number of critical ways.
-      </p>
-
-      <h3>
-        How to get involved
-      </h3>
-      <p>
-        Hope Starts Here will draw on the voices and expertise of Detroit residents, parents, caregivers, community organizations, government and business leaders. There will be many ways to participate including attending community events, sharing feedback and ideas, and joining teams to create future recommendations.
-      </p>
-      <p>
-        We want to hear your stories, learn more about your work and tap into your passion and expertise on behalf of Detroit’s youngest residents. We are scheduling individual conversations to discuss how you and your organization might be able to participate.
-      </p>
-      <p>
-        In the coming months, you can follow our progress at HopeStartsHereDetroit.org and on Facebook at fb.me/HopeStartsHereDetroit. Stay tuned for updates and information about ways you can ensure that #HopeBuilds for generations of Detroit’s youngest children.
-      </p>
-
-      <h3>
-        Contact
-      </h3>
-      <p>
-        Please contact <a href="mailto:info@hopestartsheredetroit.org">info@hopestartsheredetroit.org</a> for more information.
-      </p>
+<div class="center-text">
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <h2 class="title">About Hope Starts Here</h2>
+      <div class="page-content-wrapper">
+        <p>
+          <strong>
+            One of the best measures of a city’s well-being is how it helps children realize the nearly unlimited potential with which they are born.
+          </strong>
+        </p>
+        <hr/>
+        <img src="images/About1.png" alt=""/>
+        <img src="images/About2.png" alt=""/>
+        <hr/>
+        <img src="images/About3.png" alt=""/>
+        <p>
+          <strong>
+            Close to 300 organizations exist to support them.
+          </strong>
+        </p>
+        <img src="images/About4.png" alt=""/>
+        <br/>
+        <p>
+          <strong>
+            That means there’s positive momentum to build on.
+          </strong>
+        </p>
+        <p>
+          There are also huge opportunities for growth.
+        </p>
+        <br/>
+        <img src="images/About5.png" alt=""/>
+        <br/>
+        <img src="images/About6.png" alt=""/>
+        <p>
+          is a <strong>community engagement and strategic planning process</strong> designed to create a vision and action plan to make Detroit an equitable, world-class city for our youngest residents and their families.
+        </p>
+        <p>
+          We’re aligning and strengthening the assets and resources we already have so we can better coordinate a high-quality system that supports all of Detroit’s children and their families.
+        </p>
+        <p>
+          <strong>
+          Hope Starts Here is a unique partnership of
+          </strong>
+        </p>
+        <img src="images/About7.png" alt=""/>
+        <hr/>
+        <img src="images/About8.png" alt=""/>
+        <img src="images/About9.png" alt=""/>
+        <p>
+          <strong>
+            The following teams are the engine of Hope Starts Here’s planning process:
+          </strong>
+        </p>
+        <img src="images/About10.png" alt=""/>
+        <p>
+          <strong>
+          At its core, Hope Starts Here is really a community engagement process to collect and better utilize the knowledge and resources our community has already. Here’s what it looks like:
+          </strong>
+        </p>
+        <img src="images/About11.png" alt=""/>
+        <img src="images/About12.png" alt=""/>
+        <img src="images/About13.png" alt=""/>
+        <p>
+          <strong>
+          We want to hear your stories, learn more about your work, and tap into your passion and expertise on behalf of Detroit’s youngest residents.
+          </strong>
+        </p>
+        <p>
+          Follow us on Facebook:
+          <br/>
+          <strong>
+          @HopeStartsHereDetroit
+          </strong>
+        </p>
+        <p>
+          Use:
+          <br/>
+          <strong>
+          #HopeBuilds
+          </strong>
+        </p>
+        <p>
+          Email:
+          <br/>
+          <strong>
+          info@hopestartsheredetroit.org
+          </strong>
+        </p>
+      </div>
     </div>
   </div>
 </div>
